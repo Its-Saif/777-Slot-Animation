65ed39ec069120d39f931259caeff04410ce90ec
Its-Saif <saif41428@gmail.com>
2023-06-09 16:24:42 +0530

    Added 12 Langauges

--- index.js
+++ index.js
@@ -1,55 +1,3 @@
-// with the set time out
-// window.addEventListener("DOMContentLoaded", () => {
-// 	let increment1 = 0;
-// 	let increment2 = 0;
-// 	let increment3 = 0;
-
-// 	const carousel1 = $(".carou1");
-// 	const carousel2 = $(".carou2");
-// 	const carousel3 = $(".carou3");
-
-// 	const rotateCarousel = (carousel, increment) => {
-// 		carousel.css({
-// 			"-webkit-transform": "rotateX(" + increment * -60 + "deg)",
-// 			transform: "rotateX(" + increment * -60 + "deg)",
-// 		});
-// 		carousel.attr("data-state", (increment % 6) + 1);
-// 	};
-
-// 	const animateCarousel1 = () => {
-// 		increment1++;
-// 		rotateCarousel(carousel1, increment1);
-// 		setTimeout(animateCarousel1, 2300);
-// 	};
-
-// 	const animateCarousel2 = () => {
-// 		increment2++;
-// 		rotateCarousel(carousel2, increment2);
-// 		setTimeout(animateCarousel2, 2300);
-// 	};
-
-// 	const animateCarousel3 = () => {
-// 		increment3++;
-// 		rotateCarousel(carousel3, increment3);
-// 		setTimeout(animateCarousel3, 2300);
-// 	};
-
-// 	setTimeout(() => {
-// 		animateCarousel1();
-// 	}, 300);
-
-// 	setTimeout(() => {
-// 		animateCarousel2();
-// 	}, 600);
-
-// 	setTimeout(() => {
-// 		animateCarousel3();
-// 	}, 900);
-
-// 	// clearTimeout(animateCarousel1, animateCarousel2, animateCarousel3);
-// });
-
-// set 2 without the settimeout
 window.addEventListener("DOMContentLoaded", () => {
 	let increment1 = 0;
 	let increment2 = 0;
@@ -63,10 +11,10 @@ window.addEventListener("DOMContentLoaded", () => {
 
 	const rotateCarousel = (carousel, increment) => {
 		carousel.css({
-			"-webkit-transform": "rotateX(" + increment * -60 + "deg)",
-			transform: "rotateX(" + increment * -60 + "deg)",
+			"-webkit-transform": "rotateX(" + increment * -30 + "deg)",
+			transform: "rotateX(" + increment * -30 + "deg)",
 		});
-		carousel.attr("data-state", (increment % 6) + 1);
+		carousel.attr("data-state", (increment % 12) + 1);
 	};
 
 	const animateCarousels = (startTime) => {
